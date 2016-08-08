b1711baaa374728acced6b697681541e78bfcb5b
Valentine Silvansky <v.silvansky@gmail.com>
2016-08-08 17:05:14 +0300

    Initial Commit

---
+++ GPUImageViewBugDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUImageViewBugDemo
+//
+//  Created by Valentine on 08.08.16.
+//  Copyright © 2016 Silvansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
